177ee17769af247d0178e42a646a5e0fd3fb3c38
NikitaKx9 <nikitakx9@gmail.com>
2023-09-15 13:18:12 +0300

    FE changed

--- helpdesk/frontend/src/js/api/createRequest.js
+++ helpdesk/frontend/src/js/api/createRequest.js
@@ -1,4 +1,14 @@
 const createRequest = async (options = {}) => {
+	const resp = await fetch('http://localhost:8080/', {
+		method: 'GET',
+		data: JSON.stringify(options)
+	})
+	.then(response)
+	console.log(response)
 };
 
 export default createRequest;
+
+console.log(createRequest({
+	name: 'Isaak'
+}))
